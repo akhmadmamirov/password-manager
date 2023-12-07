d32cb36dc0a66fbf6b6019c513585ff0285248d3
Akhmadillo Mamirov <akhmadillo1@gmail.com>
2023-12-07 15:11:26 -0500

    adding form

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 from openpyxl import Workbook
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -14,37 +14,27 @@ window.config(padx=50,pady=20)
 canvas = Canvas(window, width = 200, height = 200)
 logo_img = PhotoImage(file="logo.png")
 
-def generatePassword():
+def generate_password():
     pass
 
-def register():
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+def save():
     # Get the user input from the form
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
 
-
-    # Create a new row with the user input
-    new_row = [website, email, email]
-
-    # Append the new row to the Excel sheet
-    file_path = "registration_data.xlsx"
-
-    try:
-        # Try to load the existing workbook
-        workbook = openpyxl.load_workbook(file_path)
-    except FileNotFoundError:
-        # If the file does not exist, create a new workbook
-        workbook = Workbook()
-        workbook.save(file_path)
-    # Now, you can load the newly created workbook
     
-    sheet = workbook.active
-    sheet.append(new_row)
-    workbook.save("registration_data.xlsx")
-    messagebox.showinfo("Success", "Registration successful!")
 
+    with open('data.txt', 'a') as data_file:
+        data_file.write(f'{website} | {email} | {password}\n')
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+        
+    messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email} \nPassword: {password} \nIs it ok to save?")
 
+       
+            
 canvas.create_image(100,100, image=logo_img)
 canvas.grid(row=0,column=1)
 
@@ -53,11 +43,13 @@ canvas.grid(row=0,column=1)
 website_label = Label(text="Website:", )
 website_label.grid(row=1, column=0)
 website_entry = Entry(width=38)
+website_entry.focus()
 website_entry.grid(row=1, column=1, columnspan=2)
 #Email
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 email_entry = Entry(width=38)
+email_entry.insert(0, "akhmad@email.com")
 email_entry.grid(row=2, column=1, columnspan=2)
 #Password
 password_label = Label(text="Password:")
@@ -65,10 +57,10 @@ password_label.grid(row=3, column=0)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 #Buttons
-generateP_button = Button(text="Generate Password", command=generatePassword)
+generateP_button = Button(text="Generate Password", command=generate_password)
 generateP_button.grid(row=3, column=2)
 
-register_button = Button(text="Add", command=register, width=36)
+register_button = Button(text="Add", command=save, width=36)
 register_button.grid(row=4, column=1, columnspan=2)
 
 
